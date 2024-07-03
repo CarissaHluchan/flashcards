7abb12dac82a045110787ef010ca1f6de6ee3aca
Carissa Hluchan <carissahluchan@gmail.com>
2024-07-02 18:09:47 -0600

    Add: turns.js and turns-tests

--- test/turns-test.js
+++ test/turns-test.js
@@ -8,12 +8,21 @@ describe('turns', function() {
     expect(evaluateGuess).to.be.a('function');
   });
 
-  it('should create a card and its properties', function() {
-    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    
-    expect(card.id).to.equal(1);
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-    expect(card.correctAnswer).to.equal('object');
+  it('should return correct if guess and correct answer are the same', function() {
+    const guess = 'cat\'s are cute'
+    const correctAnswer = 'cat\'s are cute'
+
+    const result = evaluateGuess(guess, correctAnswer);
+
+    expect(result).to.equal('correct');
+  });  
+
+  it('should return incorrect if guess and correct answer are not the same', function() {
+    const guess = 'cat\'s are cute'
+    const correctAnswer = 'dog\'s are cute'
+
+    const result = evaluateGuess(guess, correctAnswer);
+
+    expect(result).to.equal('incorrect');
   });  
 });
